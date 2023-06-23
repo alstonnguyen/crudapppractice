9b423a738e742e1d2f06fd5eb16c72e44c6f6b3d
Alston Nguyen <alstonnguyen@gmail.com>
2023-06-23 19:04:43 -0400

    took out mongoDB uri

--- server/model.js
+++ server/model.js
@@ -3,13 +3,11 @@
 const mongoose = require('mongoose');
 
 const mongo_URI =
-  'mongodb+srv://alstonnguyen:codesmith@cluster0.vtuodja.mongodb.net/?retryWrites=true&w=majority';
-
-//connect database
-mongoose
-  .connect(mongo_URI)
-  .then(() => console.log('You are connected'))
-  .catch((err) => console.log(err));
+  //connect database
+  mongoose
+    .connect(mongo_URI)
+    .then(() => console.log('You are connected'))
+    .catch((err) => console.log(err));
 
 //write schemas
 //this imports schemas
